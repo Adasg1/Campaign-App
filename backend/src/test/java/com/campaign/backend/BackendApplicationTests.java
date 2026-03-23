a4bfc2afa220f388bd0a29b891b83fd76156a3b9
Adasg1 <adas.glowacki@interia.eu>
2026-03-24 00:06:13 +0100

    Initial commit

---
+++ backend/src/test/java/com/campaign/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campaign.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
